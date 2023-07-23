dace18721ddf963fbb2044d6e595c0bc5944d794
Max Zhakun <maxzhakun@quantifycrypto.net>
2023-07-22 20:30:01 -0500

    changed docker file and bitbucket configs

---
+++ types/graph.ts
@@ -0,0 +1,30 @@
+export interface EvmTransaction {
+  chainId: number
+  from: string
+  gasFees: number
+  gssLimit: number
+  hash: string
+  isPending: boolean
+  status: string
+  nonce: number
+  to: string
+  txDataHex: string
+  value: string
+  Input: {
+    methodSigDataStr: string
+    inputsSigDataStr: string
+    inputsMap: Object | null
+    argsMap: Object | null
+  }
+  logEvents: {
+    items: {
+      network: string
+      contract: string
+      name: string
+      topic: string
+      address: string
+      signature: string
+      allFunctionParams: Object | null
+    }[]
+  }
+}
