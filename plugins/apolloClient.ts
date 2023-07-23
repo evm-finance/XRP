93fd4d4a355ab4a5afc449ef9a2bd5d3572af712
Max Zhakun <maxzhakun@quantifycrypto.net>
2023-07-23 03:00:57 -0500

    changed docker file and bitbucket configs

--- plugins/apolloClient.ts
+++ plugins/apolloClient.ts
@@ -1,3 +1,4 @@
+/*
 import { provide, onGlobalSetup, defineNuxtPlugin } from '@nuxtjs/composition-api'
 import { DefaultApolloClient } from '@vue/apollo-composable/dist'
 // import { onError } from '@apollo/client/link/error'
@@ -7,7 +8,7 @@ export default defineNuxtPlugin(({ app }) =>
 
     provide(DefaultApolloClient, app.apolloProvider?.defaultClient)
 
-    /*   const link = onError(({ graphQLErrors, networkError }) => {
+    /!*   const link = onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
@@ -16,6 +17,21 @@ export default defineNuxtPlugin(({ app }) =>
       if (networkError) console.log(`[Network error]: ${networkError}`)
     })
 
-    console.log(link) */
+    console.log(link) *!/
   })
 )
+*/
+
+import { Context } from '@nuxt/types'
+import { provide, onGlobalSetup, defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+/**
+ * This plugin will connect @nuxt/apollojs with @vue/apollo-composable
+ */
+
+export default defineNuxtPlugin(({ app }: Context): void => {
+  onGlobalSetup(() => {
+    provide(DefaultApolloClient, app.apolloProvider?.defaultClient)
+  })
+})
